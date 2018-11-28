269cb0c04334c773c42d1876374a27aacba380da
Andrey Sokolow <quick.sokolow94@gmail.com>
2018-11-28 19:01:40 +0300

    LK KVSSPB first-screen completed

--- app/js/myscripts.js
+++ app/js/myscripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-	alert('hello');
+	console.log('Hello there!');
+	$('.select').SumoSelect()
+
+	$(document).on('click', '.input__checkbox', function() {
+		var img, imgsrc,
+			checkbox = $(this).find('input[type="checkbox"]');
+
+		if ($(this).parent('.input-with-edit').find('.email-switcher').length > 0) {
+			img = $('.email-switcher');
+			imgsrc = img.attr('src');
+			if (imgsrc == 'img/email.svg' && !checkbox.is(':checked')) {
+				img.attr('src', 'img/email-off.svg');
+			} else {
+				img.attr('src', 'img/email.svg');
+			}
+		} 
+		if ($(this).parent('.input-with-edit').find('.phone-switcher').length > 0) {
+			img = $('.phone-switcher');
+			imgsrc = img.attr('src');
+			if (imgsrc == 'img/phone.svg' && !checkbox.is(':checked')) {
+				img.attr('src', 'img/phone-off.svg');
+			} else {
+				img.attr('src', 'img/phone.svg');
+			}
+		}
+
+	})
 });
